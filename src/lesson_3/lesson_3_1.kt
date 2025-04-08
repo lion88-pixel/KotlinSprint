c62df9880ed1e6dea8eeccf409eb55a1a1f47370
Александр <lion_alex1988@mail.ru>
2025-04-08 22:09:26 +0300

    KS-3-1 correction

---
+++ src/lesson_3/lesson_3_1.kt
@@ -0,0 +1,12 @@
+package lesson_3
+
+fun main() {
+    val name = "Александр!"
+    var greeting: String
+
+    greeting = "Доброе утро"
+    println("$greeting $name")
+
+    greeting = "Добрый вечер"
+    println("$greeting $name")
+}
